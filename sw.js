698f8fd739641d14374f534fb13080e576d56b61
hasansohaginfo <hasansohag.info@gmail.com>
2026-08-01 13:01:54 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,35 @@
-const CACHE_NAME = 'result-app-v1';
-const assets = [
+const CACHE_NAME = 'result-soft-v2';
+const urlsToCache = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('./index.html'));
     })
   );
 });
